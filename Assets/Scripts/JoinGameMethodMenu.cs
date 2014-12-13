00639382909361bbf450d74cf614da1011dac67b
lucaho0213 <luchao_boy@163.com>
2014-12-13 23:31:17 +0800

    Modify build settings
    给build setting里面加了sence的序号

---
+++ Assets/Scripts/JoinGameMethodMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinGameMethodMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI ()
+	{
+		if(GUI.Button(new Rect(10,10,50,50),"创建房间")){
+			Debug.Log("creat");
+			Application.LoadLevel("RoomSettings");
+		}
+	}
+
+}
